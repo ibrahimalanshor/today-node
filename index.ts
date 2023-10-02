99c9c6fdb73d0324a246d7047b3e83f56a5baa6d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-02 21:03:05 +0700

    setup middlewares

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { App } from 'ekspres';
-import { routes } from './src/routes';
+import { routes } from './src/app/routes';
+import { middlewares } from './src/app/middlewares';
 
 const app = new App();
 
+app.setMiddlewares(middlewares);
 app.setRoutes(routes);
 app.listen((port) => console.log(`app listening at ${port}`));
