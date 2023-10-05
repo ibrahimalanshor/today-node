5734f1aac9b71379c13feaa8ec54ab43e1dce54d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-06 06:14:55 +0700

    feat: create todo handler

--- src/modules/todo/todo.handler.ts
+++ src/modules/todo/todo.handler.ts
@@ -1,7 +1,13 @@
+import { RouterContext } from 'ekspres';
 import { TodoModel } from './model/todo.model';
 
 export class TodoHandler {
   async getAll() {
     return await TodoModel.paginate();
+  }
+  async create(context: RouterContext) {
+    return await TodoModel.create({
+      name: context.req.body,
+    });
   }
 }
